8f5a8741fa750723f40f15f46a193125eaa6996a
Andre Azzolini <apazzolini@gmail.com>
2015-09-22 14:50:33 -0500

    Switch npm config to use JSON5

--- src/api/db.js
+++ src/api/db.js
@@ -1,8 +1,11 @@
 import pmongo from 'promised-mongo';
 import config from 'config';
 
-// const auth = `${config.get('mongo.user')}:${config.get('mongo.pass')}`;
-// const db = pmongo(`${auth}@${config.get('mongo.host')}/${config.get('mongo.db')}`);
+let con = '';
+if (config.has('mongo.user')) {
+  con = config.get('mongo.user') + ':' + config.get('mongo.pass') + '@';
+}
+con += config.get('mongo.host') + '/' + config.get('mongo.db');
 
-const db = pmongo('localhost/andreazzolini');
+const db = pmongo(con);
 export default db;
