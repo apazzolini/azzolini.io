e293ec1a96ed74af8e9ff306a2d47b315b4e2904
Andre Azzolini <apazzolini@gmail.com>
2016-01-10 01:43:15 -0600

    Cleanup NavBar to use arrow functions for better `this` usage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,26 +17,19 @@ export default class NavBar extends Component {
 
     // Delay the binding in case the browser is restoring scroll position
     setTimeout(() => {
-      this.boundHandleScroll = this.handleScroll.bind(this);
-      this.boundDetermineExpandingMenu = this.determineExpandingMenu.bind(this);
-
-      window.addEventListener('scroll', this.boundHandleScroll);
-      window.addEventListener('resize', this.boundDetermineExpandingMenu);
+      window.addEventListener('scroll', this.handleScroll);
+      window.addEventListener('resize', this.determineExpandingMenu);
     }, 1000);
   }
 
   componentWillUnmount() {
-    if (typeof this.boundHandleScroll !== 'undefined') {
-      window.removeEventListener('scroll', this.boundHandleScroll);
-    }
-
-    if (typeof this.boundDetermineExpandingMenu !== 'undefined') {
-      window.removeEventListener('resize', this.boundDetermineExpandingMenu);
-    }
+    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('resize', this.determineExpandingMenu);
   }
 
-  handleScroll(event) {
+  handleScroll = (event) => {
     const pos = event.srcElement.body.scrollTop;
+
     this.setState({
       ...this.state,
       scrollY: pos,
@@ -45,23 +38,25 @@ export default class NavBar extends Component {
     });
   }
 
-  determineExpandingMenu() {
+  determineExpandingMenu = () => {
     // If we've determined this is a mobile browser, we want to use an expanding
     // menu with animation. This must be determined client-side.
     this.isExpandingMenu = typeof window !== 'undefined' && window.innerWidth < 550;
+
     if (this.isExpandingMenu) {
       this.setState({...this.state, menuExpanded: false});
-      this.forceUpdate();
     } else {
       this.setState({...this.state, menuExpanded: true});
     }
+
+    this.forceUpdate();
   }
 
-  collapseMenu() {
+  collapseMenu = () => {
     this.setState({ ...this.state, menuExpanded: false });
   }
 
-  toggleMenuExpanded(expanded) {
+  toggleMenuExpanded = () => {
     this.setState({ ...this.state, menuExpanded: !this.state.menuExpanded });
   }
 
@@ -76,19 +71,19 @@ export default class NavBar extends Component {
     return (
       <nav className={'MainNav ' + mainNavAnimation}>
         <div className="container">
-          <Link to="/" onClick={() => this.collapseMenu()}>Andre Azzolini</Link>
+          <Link to="/" onClick={this.collapseMenu}>Andre Azzolini</Link>
 
           <ul className={'u-pull-right ' + (this.isExpandingMenu ? 'expanding-menu' : '')}>
             { this.isExpandingMenu &&
               <li className="expand-icon">
-                <a onClick={() => this.toggleMenuExpanded()}>
+                <a onClick={this.toggleMenuExpanded}>
                   <span></span>
                 </a>
               </li>
             }
 
             <div className={(this.isExpandingMenu && !this.state.menuExpanded ? 'u-hidden' : '')}>
-              <li><Link to="/about" onClick={() => this.collapseMenu()}>About</Link></li>
+              <li><Link to="/about" onClick={this.collapseMenu}>About</Link></li>
               <li><a href="https://www.github.com/apazzolini">GitHub</a></li>
               <li><a href="https://twitter.com/apazzolini">Twitter</a></li>
             </div>
