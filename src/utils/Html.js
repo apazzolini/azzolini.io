58301e838e752fb92e3cd6515106605099ec8fed
Andre Azzolini <apazzolini@gmail.com>
2016-01-10 00:55:11 -0600

    Add favicon

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -28,6 +28,8 @@ export default class Html extends Component {
         <head>
           <meta charSet="utf-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
           {DocumentMeta.renderAsReact()}
 
