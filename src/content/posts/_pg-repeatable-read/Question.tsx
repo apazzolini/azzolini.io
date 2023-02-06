a08a9b4fe3a176bc901fa43651f556fb2d1713a9
Andre Azzolini <andre@azzolini.io>
2023-02-05 18:11:34 -0700

    Add PG repeatable read nuances post

---
+++ src/content/posts/_pg-repeatable-read/Question.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'preact/compat';
+
+export type QuestionProps = {
+  q: string;
+  answers: string[];
+  rightAnswer: number;
+  answerDescription: string;
+};
+
+export default function Question({ q, answers, rightAnswer, answerDescription }: QuestionProps) {
+  const [answer, setAnswer] = useState<number | null>(null);
+
+  return (
+    <div className="">
+      <div className="mb-4">{q}</div>
+
+      {answers.map((a, idx) => {
+        return (
+          <button
+            className={`
+
+              border border-rose-450 py-2 px-4 rounded mr-3 border-opacity-75
+              ${idx === answer ? 'bg-rose-450' : ''}
+              ${
+                answer !== null && answer !== rightAnswer && idx === rightAnswer ? 'ring-rose-450 ring-2' : ''
+              }
+            `}
+            key={idx}
+            disabled={answer !== null}
+            onClick={() => setAnswer((prev) => (prev === null ? idx : prev))}
+          >
+            {a}
+          </button>
+        );
+      })}
+
+      {answer !== null ? (
+        <div className="mt-4">{answer === rightAnswer ? 'Correct!' : 'Bummer :('}</div>
+      ) : null}
+      {answer !== null && <div className="mt-4 bg-zinc-800 py-1 px-2 rounded">{answerDescription}</div>}
+    </div>
+  );
+}
