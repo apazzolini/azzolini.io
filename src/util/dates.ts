ecd5d7e2e6a891ccaec569bf73f3c9ea64313e0c
Andre Azzolini <andre@azzolini.io>
2023-02-20 23:29:47 -0700

    Parse dates in UTC

--- src/util/dates.ts
+++ src/util/dates.ts
@@ -1,7 +1,6 @@
 export function formatDateLong(date: Date): string {
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const day = date.getDate();
-  const year = date.getFullYear();
-
+  const month = date.toLocaleString('UTC', { month: 'long', timeZone: 'UTC' });
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
   return `${month} ${day}, ${year}`;
 }
