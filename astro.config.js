89bed83d5778631c3fb3cc92c5f8a70483f60a16
Andre Azzolini <andre@azzolini.io>
2024-03-15 13:59:17 -0600

    Disable prefecthing

--- astro.config.js
+++ astro.config.js
@@ -19,6 +19,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: false,
   site: 'https://azzolini.io',
   integrations: [
     tailwind(),
